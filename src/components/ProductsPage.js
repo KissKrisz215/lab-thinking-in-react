4ec0166cb4d66391ae2e72e6a50a97ca18abf203
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-05-29 15:08:11 +0200

    Finished Iteration 5

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -5,22 +5,40 @@ import { ProductTable } from './ProductTable';
 
 export function ProductsPage(){
     const [products, setProducts] = useState(jsonData);
+    const [inStock, setInStock] = useState(false);
+
+    const filterProducts = (value) => {
+       if(value === null && inStock === false){
+        const productsCopy = [...products];
+        const filteredProducts = productsCopy.filter((product) => product.inStock === true);
+        setProducts(filteredProducts);  
+       }else if(value === null & inStock === true){
+        setProducts(jsonData);  
+       }else if(value.length === 0 && inStock === true){
+            const productsCopy = [...jsonData];
+            const filteredProducts = productsCopy.filter((product) => product.inStock);
+            setProducts(filteredProducts);   
+       }else{
+            const productsCopy = [...jsonData];
+            const filteredProducts = productsCopy.filter((product) => product.name.toLowerCase().includes(value.toLowerCase()) || product.price.includes(value));
+            setProducts(filteredProducts); 
+       }
+    }
 
     const searchProduct = (event) => {
         const searchValue = event.target.value;
-        if(searchValue.length === 0){
-            setProducts(jsonData);
-        }else{
-            const productsCopy = [...products];
-            const filteredProducts = productsCopy.filter((product) => product.name.toLowerCase().includes(searchValue.toLowerCase()) || product.price.includes(searchValue));
-            setProducts(filteredProducts);   
-        }
+        filterProducts(searchValue)
     }
-    
+
+    const showInStock = () => {
+        setInStock(!inStock);
+        filterProducts(null)
+    }
+
     return(
      <div className='container'>
         <h1>IronStore</h1>
-        <SearchBar searchProduct={searchProduct}  />
+        <SearchBar showInStock={showInStock} searchProduct={searchProduct}  />
         <ProductTable products={products} setProducts={setProducts} />
      </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,11 @@
-export function SearchBar({searchProduct}){
+export function SearchBar({searchProduct, showInStock}){
 
     return(
         <div className="container">
             <label htmlFor="search">Search</label>
             <input onChange={(event) => searchProduct(event)} className="form-control" type="text" id="search" />
-            <input type="checkbox" name="" id="" />
-            <label className="mx-2" htmlFor="">Only Show Products In Stock</label>
+            <input onChange={showInStock} type="checkbox" name="checkbox" id="checkbox" />
+            <label className="mx-2" htmlFor="checkbox">Only Show Products In Stock</label>
         </div>
     );
 }
